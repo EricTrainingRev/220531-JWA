7bf9881310e8bc080c9b38bae4a3a0061e148e6c
EricTrainingRev <eric.suminski@revature.com>
2022-06-07 17:51:51 -0400

    added more javascript exercises for arrays and objects

---
+++ Exercises/code-writing/code-writing-challenges-6.js
@@ -0,0 +1,32 @@
+/*
+    The code writing challenges in this module are designed to help you become more familiar with how the different
+    control flow tools available to you work, and how different data structures work. For each challenge you will 
+    be given a snipet of code and instructions for what you need to do to make the code work.
+*/
+
+/*
+    Exercise One: assign an empty array to the variable "myArray" below
+*/
+
+let myArray;
+/*
+    Exercise Two: add a string value and a number value to the empty array you created above. HINT: arrays have a 
+    built in method to handle this
+*/
+
+// write code here
+
+/*
+    Exercise Three: print the numeric and string values that you added to the array above by referencing their
+    index positions
+*/
+
+console.log(/* print one value here */);
+console.log(/* print the other value here */);
+
+/*
+    Exercise Four: remove the string element from the array in a way that makes it so only the number element
+    remains in the array. HINT: arrays have built in methods of removing the first and last elements in the array
+*/
+
+// write code here
---
+++ Exercises/code-writing/code-writing-challenges-7.js
@@ -0,0 +1,37 @@
+/*
+    The code writing challenges in this module are designed to help you become more familiar with how the different
+    control flow tools available to you work, and how different data structures work. For each challenge you will 
+    be given a snipet of code and instructions for what you need to do to make the code work.
+*/
+
+/*
+    Exercise One: assign an empty object to the variable "myObject" below
+*/
+
+let myObject;
+/*
+    Exercise Two: add a string value and a number value to the empty object you created above. Make the key for
+    the numeric value a number as well. HINT: objects have two ways of creating new key/value pairs
+*/
+
+// write code here
+
+/*
+    Exercise Three: print the numeric and string values that you added to the object above. HINT: objects have two
+    ways of accessing their keys
+*/
+
+console.log(/* print one value here */);
+console.log(/* print the other value here */);
+
+/*
+    Exercise Four: remove the string element from the array in a way that makes it so only the number element
+    remains in the array. HINT: arrays have built in methods of removing the first and last elements in the array
+*/
+
+function objectFunction(){
+    console.log("this should print to the console second")
+}
+// assign the function to a key on this line
+console.log("this should print to the console first")
+// call the function from myObject on this line
