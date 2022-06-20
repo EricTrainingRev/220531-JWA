ad2354d135a8b267449cbe1ca8747e8bce866261
EricTrainingRev <eric.suminski@revature.com>
2022-06-20 17:08:22 -0400

    added some java code writing challenges

---
+++ Exercises/java-code-writing/JavaCodeWritingChallengeOne.java
@@ -0,0 +1,48 @@
+public class JavaCodeWritingChallengeOne{
+        /*
+            The code writing challenges in this folder are designed to help you become more familiar with how the different
+            control flow tools available to you work in Java. For each challenge you will be given a snipet of code and instructions
+            for what you need to do to make the code work. Make sure to remove the false boolean in each statement when
+            working on the exercises.
+         */
+    public static void main(String[] args) {
+                
+        /*
+            Exercise One: create a variable called myBoolean, assign it a boolean value, and use it to make the if 
+            statement below execute
+        */
+
+        if(false){
+            System.out.println("If you can see this in the console then you are done with exercise one");
+        }
+
+        /*
+            Exercise Two: create two variables, one called numOne and the other numTwo, and assign them both numeric values.
+            Use them to make the if statement below execute. HINT: you will need to use one or more logical operators
+        */
+
+        if(false){
+            System.out.println("if you can see this in the console then you are done with exercise two");
+        }
+
+        /*
+            Exercise Three: create a variable called myString and assign it a string value (remember to use either single
+            quotes or double quotes). Use the variable you created and the equals() method to make the
+            if statement below execute
+        */
+
+        if(false){
+            System.out.println("if you can see this in the console then you are done with exercise three");
+        }
+
+        /*
+            Exercise Four: create a variable called myNumThree and assign it an int value. Then create a variable called
+            myStringTwo and assign it the same value but as a string. Use these two variables to make the if statement execute
+            HINT: you will need to find some way to convert one data type to another
+        */
+
+        if(false){
+            System.out.println("if you can see this in the console then you are done with exercise four");
+        }
+    }
+}
---
+++ Exercises/java-code-writing/JavaCodeWritingChallengeTwo.java
@@ -0,0 +1,56 @@
+public class JavaCodeWritingChallengeTwo {
+    public static void main(String[] args) {
+        /*
+            The code writing challenges in this module are designed to help you become more familiar with how the different
+            control flow tools available to you work. For each challenge you will be given a snipet of code and instructions
+            for what you need to do to make the code work. Make sure to remove the true boolean in each statement when
+            working on the exercises.
+        */
+
+        /*
+            Exercise One: create a variable called myBoolean, assign it a boolean value,  and use it to make the else 
+            statement below execute
+        */
+
+        if(true){
+            System.out.println("If you can see this you have NOT completed exercise one");
+        } else {
+            System.out.println("if you can see this you are done with exercise one");
+        }
+
+        /*
+            Exercise Two: create two variables, one called numOne and the other numTwo, and assign them both int values.
+            Use them to make the else statement below execute. HINT: you will need to use one or more logical operators
+        */
+
+        if(true){
+            System.out.println("If you can see this you have NOT completed exercise two");
+        } else {
+            System.out.println("if you can see this you are done with exercise two");
+        }
+
+        /*
+            Exercise Three: create a variable called myString and assign it a string value (remember to use either single
+            quotes or double quotes). Use the variable you created to make the else statement below execute HINT: comparisons
+            for Strings in Java are made with the .equals() method
+        */
+
+        if(true){
+            System.out.println("If you can see this you have NOT completed exercise three");
+        } else {
+            System.out.println("if you can see this you are done with exercise three");
+        }
+
+        /*
+            Exercise Four: create a variable called myNumThree and assign it an int value. Then create a variable called
+            myStringTwo and assign it a different number value but as a string. Use these two variables `to make 
+            the else statement execute
+        */
+
+        if(true){
+            System.out.println("If you can see this you have NOT completed exercise four");
+        } else {
+            System.out.println("if you can see this you are done with exercise four");
+        }
+    }   
+}
