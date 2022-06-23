881365516546a43a8360cade0851e696ac7d07e3
EricTrainingRev <eric.suminski@revature.com>
2022-06-23 14:58:40 -0400

    finished automating manager and tester login

--- Week4/Day4/Login-Automation-Project-Goes-Here/bugcatcherautomation/src/test/java/com/automation/runner/TestRunner.java
+++ Week4/Day4/Login-Automation-Project-Goes-Here/bugcatcherautomation/src/test/java/com/automation/runner/TestRunner.java
@@ -1,6 +1,15 @@
 package com.automation.runner;
 
+import java.time.Duration;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.automation.poms.Login;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
@@ -8,5 +17,24 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features="classpath:features", glue="com/automation/steps")
 public class TestRunner {
+
+    public static WebDriver driver;
+    public static Login login;
+    public static WebDriverWait wait;
+
+    @BeforeClass // this makes the method execute before all the steps
+    public static void setup(){
+        // make sure to set the value for the key as the full relative path to the chromedriver.exe
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        login = new Login(driver);
+        // the WebDriverWait is used to tell Selenium to wait up to a set amount of time for a given condition
+        wait = new WebDriverWait(driver, 2);
+    }
+
+    @AfterClass // this makes the method execute after all the steps
+    public static void teardown(){
+        driver.quit();
+    }
     
 }
--- Week4/Day4/Login-Automation-Project-Goes-Here/bugcatcherautomation/src/test/java/com/automation/steps/LoginSteps.java
+++ Week4/Day4/Login-Automation-Project-Goes-Here/bugcatcherautomation/src/test/java/com/automation/steps/LoginSteps.java
@@ -1,5 +1,10 @@
 package com.automation.steps;
 
+import org.junit.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import com.automation.runner.TestRunner;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -12,14 +17,13 @@ public class LoginSteps {
 
     @Given("the employee is on the login page")
     public void the_employee_is_on_the_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        // when telling selenium to get a local file you have to add File:// to the start of the url
+        TestRunner.driver.get("File://C:/Users/EricSuminski/Desktop/220531-JWA/Week4/Day4/Login-Automation-Project-Goes-Here/bugcatcherautomation/src/test/resources/web-pages/login.html");
     }
 
     @When("the employee clicks the login button")
     public void the_employee_clicks_the_login_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        TestRunner.login.clickButton();
     }
 
 
@@ -29,20 +33,20 @@ public class LoginSteps {
 
     @When("the manager enters his correct username")
     public void the_manager_enters_his_correct_username() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        TestRunner.login.enterUsername("Manager");
     }
 
     @When("the manager enters his correct password")
     public void the_manager_enters_his_correct_password() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        TestRunner.login.enterPassword("Automation");
     }
 
     @Then("the manager should be logged in to the manager home page")
     public void the_manager_should_be_logged_in_to_the_manager_home_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        // this method checks that my simulated manager has actually ended up on the manager page
+        TestRunner.wait.until(ExpectedConditions.titleIs("Manager Page"));
+        String title = TestRunner.driver.getTitle();
+        Assert.assertEquals("Manager Page", title);
     }
 
     /*
@@ -51,20 +55,19 @@ public class LoginSteps {
     
     @When("the tester enters his correct username")
     public void the_tester_enters_his_correct_username() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        TestRunner.login.enterUsername("Tester");
     }
 
     @When("the tester enters his correct password")
     public void the_tester_enters_his_correct_password() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        TestRunner.login.enterPassword("Automation");
     }
     
     @Then("the tester should be logged in to the tester home page")
     public void the_tester_should_be_logged_in_to_the_tester_home_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+        TestRunner.wait.until(ExpectedConditions.titleIs("Tester Page"));
+        String title = TestRunner.driver.getTitle();
+        Assert.assertEquals("Tester Page", title);
     }
     
     
