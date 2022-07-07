e2148677f9b9b05d4e9d940af6b251d419ebdf36
RoryEiffe <roryeiffe8@gmail.com>
2022-07-07 12:35:17 -0400

    Pushing update to App.java

--- Week6/Day3/hibernate-demo/hibernate-demo/src/main/java/com/revature/App.java
+++ Week6/Day3/hibernate-demo/hibernate-demo/src/main/java/com/revature/App.java
@@ -1,5 +1,7 @@
 package com.revature;
 
+import com.revature.entity.Pet;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        Pet pet = new Pet(1, "Ashes", "Cat", "grey", 10, 4, "Maine Coone");
+        System.out.println(pet.toString());
     }
 }
